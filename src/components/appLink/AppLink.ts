85b97416d43d5ab9d13b50b5a5e3f00e5bdd5b2b
Gennady Kashin <gennady.kashin94@gmail.com>
2023-09-27 09:45:43 +0300

    fix: try to fix applink 3

--- src/components/appLink/AppLink.ts
+++ src/components/appLink/AppLink.ts
@@ -1,7 +1,7 @@
 import Block from '../../core/Block';
 import { AppLinkProps } from '../../types';
-import template from './AppLink.hbs?raw';
-import './AppLink.scss';
+import template from './appLink.hbs?raw';
+import './appLink.scss';
 
 export class AppLink extends Block {
     constructor(props: AppLinkProps) {
